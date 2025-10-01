6a25a840309c8570bdcd8eeb48d189f2c034bf6a
himalayashinde@gmail.com <himalayashinde@gmail.com>
2025-10-01 16:38:19 +0530

    TestNG Annotations

--- TestNGTutorial/src/test/java/TestNG/demo3.java
+++ TestNGTutorial/src/test/java/TestNG/demo3.java
@@ -1,9 +1,28 @@
 package TestNG;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class demo3 {
+	
+	
+	@BeforeSuite
+	public void beforeSuiteMethod() {
+		System.out.println("Inside beforeSuiteMethod method\n");
+	}
 
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Inside beforeMethod method\n");
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("Inside afterMethod method\n");
+	}
+	
 	@Test
 	public void WebLoginCarLoan() {
 		System.out.println("WebLoginCarLoan method");
