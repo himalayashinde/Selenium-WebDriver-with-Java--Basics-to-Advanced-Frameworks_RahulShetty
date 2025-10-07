a1ab167a1f1ec1368bec7e1e3d89767c0425a232
himalayashinde@gmail.com <himalayashinde@gmail.com>
2025-10-07 23:22:36 +0530

    Section 18 Completed

--- SeleniumFrameworkDesign/src/test/java/RahulShettyAcademy/StandAloneTest.java
+++ SeleniumFrameworkDesign/src/test/java/RahulShettyAcademy/StandAloneTest.java
@@ -7,8 +7,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class StandAloneTest {
 
@@ -20,7 +22,8 @@ public class StandAloneTest {
 		String url = "https://rahulshettyacademy.com/client";
 		String user = "himalayashinde@gmail.com";
 		String password = "Himalaya@1234";
-
+		String productName= "iphone 13 pro";
+		
 		By userEmail = By.id("userEmail");
 		By userPassword = By.id("userPassword");
 		By loginbtn = By.id("login");
@@ -40,7 +43,7 @@ public class StandAloneTest {
 		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
 
 		WebElement prod = products.stream()
-						  .filter(product -> product.findElement(By.cssSelector("b")).getText().equalsIgnoreCase("iphone 13 pro")).findFirst()
+						  .filter(product -> product.findElement(By.cssSelector("b")).getText().equalsIgnoreCase(productName)).findFirst()
 						  .orElse(null);
 
 		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
@@ -55,9 +58,49 @@ public class StandAloneTest {
 		driver.findElement(By.cssSelector("[routerlink*=cart]")).click();
 		
 		
+		List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
+		
+		boolean match = cartProducts.stream().anyMatch(cartitem -> cartitem.getText().equalsIgnoreCase(productName));
+		
+		Assert.assertTrue(match);
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector("#toast-container"))));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".totalRow button")));
+		
+		driver.findElement(By.cssSelector(".totalRow button")).click();
+		
+		
+		driver.findElement(By.cssSelector("input[placeholder='Select Country']")).sendKeys("ind");
+		
+		
+		
+	
+		List<WebElement> countries = driver.findElements(By.cssSelector(".ta-results button span"));
+		
+		for(WebElement country : countries) {
+			
+			String countryName = country.getText().trim();
+			
+			System.out.println("Found country: " + countryName);
+			
+			if(countryName.equalsIgnoreCase	("India")) {
+				country.click();
+				 break;
+			}
+		}		
+
+		Actions action = new Actions(driver);
+		
+		action.moveToElement(driver.findElement(By.cssSelector(".action__submit"))).build().perform();
+		
+		driver.findElement(By.cssSelector(".action__submit")).click();
+		
+		String confimOrderMsg = driver.findElement(By.cssSelector(".hero-primary")).getText();
+		
+		//	Assert.assertEquals( ," Thankyou for the order. ");
+		Assert.assertTrue(confimOrderMsg.equalsIgnoreCase("Thankyou for the order."));
 		
 		Thread.sleep(3000);
-		//driver.quit();
+		driver.quit();
 	}
 
 }
